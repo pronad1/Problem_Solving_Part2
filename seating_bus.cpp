609c156280777d47ee9be70574e8057bed8b713f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-13 21:19:33 +0600

    8/13/24

---
+++ seating_bus.cpp
@@ -0,0 +1,57 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll c=0,n;
+    cin>>n;
+    map<ll,ll>mp;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    mp[v[0]]++;
+    for (int i = 1; i < n; i++)
+    {
+        if (mp[v[i]-1]||mp[v[i]+1])
+        {
+            mp[v[i]]++;
+        }
+        else
+        {
+            cout<<"NO"<<'\n';
+            return;
+        }
+        
+    }
+    cout<<"YES"<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
