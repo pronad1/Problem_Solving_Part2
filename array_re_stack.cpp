f99324a347bf7e3cbf7c77f21ba7e653bb65c68a
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-19 15:27:38 +0600

    8/19/24

--- array_re_stack.cpp
+++ array_re_stack.cpp
@@ -10,10 +10,36 @@ using ll=long long ;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
-
+void push(int stack[],int top,int maxst,int item){
+    if (top==maxst)
+    {
+        cout<<"OVERFLOW"<<'\n';
+        return;
+    }
+    else
+    {
+        top++;
+        stack[top]=item;
+    }
+    
+}
 void solve(void)
 {
-
+    int top=0,n;
+    cin>>n;
+    int stack[n];
+    int maxst=n;
+    for (int i = 0; i <n; i++)
+    {
+        int item;
+        cin>>item;
+        push(stack,top,maxst,item);
+    }
+    for (int i = 0; i <n; i++)
+    {
+        cout<<stack[i]<<'\n';
+    }
+    
 }
 
 
@@ -24,7 +50,7 @@ ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
   ll t=1;
-  cin>>t;
+  //cin>>t;
     while(t--)
      {
        solve();
