47129c508f99e89e98c47e0aaa559ab005df0ad7
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-20 21:12:53 +0600

    8/20/24

--- test.cpp
+++ test.cpp
@@ -5,27 +5,46 @@ using namespace std;
 int main() {
     int t;
     cin >> t;
-    
     while (t--) {
         int n;
         cin >> n;
-        
-        if (n % 2 == 0) {
-            cout << -1 << endl;
-        } else {
-            vector<int> permutation(n);
-            int left = 1;
-            int right = n;
-            for (int i = 0; i < n; i += 2) {
-                permutation[i] = left++;
-                permutation[i + 1] = right--;
-            }
-            
-            for (int i = 0; i < n; ++i) {
-                cout << permutation[i] << " ";
-            }
-            cout << endl;
+
+        vector<pair<int, int>> edges;
+        vector<int> parent(n + 1, 0);
+
+        for (int i = 2; i <= n; ++i) {
+            // Initially assume the first node as root
+            cout << "? 1 " << i << endl;
+            cout.flush();
+            int x;
+            cin >> x;
+            // The response node x should be connected to node i
+            edges.emplace_back(x, i);
+            parent[i] = x;
         }
+
+        // Now, connect the rest of the nodes
+        for (int i = 2; i <= n; ++i) {
+            for (int j = i + 1; j <= n; ++j) {
+                cout << "? " << i << " " << j << endl;
+                cout.flush();
+                int x;
+                cin >> x;
+                if (x == i || x == j) {
+                    if (parent[i] != j && parent[j] != i) {
+                        edges.emplace_back(i, j);
+                    }
+                }
+            }
+        }
+
+        // Output the found edges
+        cout << "!";
+        for (auto& edge : edges) {
+            cout << " " << edge.first << " " << edge.second;
+        }
+        cout << endl;
+        cout.flush();
     }
     
     return 0;
