df3a6bae2a99898d2f120430a776bd2a05703e16
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-22 16:56:44 +0600

    8/22/24

--- test.cpp
+++ test.cpp
@@ -1,50 +1,33 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+// Function to calculate minimum number of operations
+int minOperations(int l, int r) {
+    int operations = 0;
+    while (r > 0) {
+        if (r % 3 == 0) {
+            r /= 3;
+        } else {
+            r -= 1;
+        }
+        operations++;
+    }
+    return operations;
+}
 
 int main() {
     int t;
     cin >> t;
+    
     while (t--) {
-        int n;
-        cin >> n;
-
-        vector<pair<int, int>> edges;
-        vector<int> parent(n + 1, 0);
-
-        for (int i = 2; i <= n; ++i) {
-            // Initially assume the first node as root
-            cout << "? 1 " << i << endl;
-            cout.flush();
-            int x;
-            cin >> x;
-            // The response node x should be connected to node i
-            edges.emplace_back(x, i);
-            parent[i] = x;
-        }
-
-        // Now, connect the rest of the nodes
-        for (int i = 2; i <= n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
-                cout << "? " << i << " " << j << endl;
-                cout.flush();
-                int x;
-                cin >> x;
-                if (x == i || x == j) {
-                    if (parent[i] != j && parent[j] != i) {
-                        edges.emplace_back(i, j);
-                    }
-                }
-            }
-        }
-
-        // Output the found edges
-        cout << "!";
-        for (auto& edge : edges) {
-            cout << " " << edge.first << " " << edge.second;
-        }
-        cout << endl;
-        cout.flush();
+        int l, r;
+        cin >> l >> r;
+        
+        // Calculate the operations needed for r to become 0
+        int result = minOperations(l, r);
+        
+        // Output the result
+        cout << result << endl;
     }
     
     return 0;
