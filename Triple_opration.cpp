5c9506b4eba2bad17ab88a77b33ee9e00cd972a4
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-22 17:02:50 +0600

    8/22/24

--- Triple_opration.cpp
+++ Triple_opration.cpp
@@ -10,7 +10,19 @@ using ll=long long ;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
+const int MAX = 200'007;
 
+int a[MAX], psum[MAX];
+
+int f(int x){
+    int c=0;
+    while (x)
+    {
+        x/=3;
+        c++;
+    }
+    return c;
+}
 void solve(void)
 {
     ll l,r;
@@ -24,6 +36,16 @@ int main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
+psum[0]=0;
+for (int i = 1; i < MAX-1; i++)
+{
+    a[i]=f(i);
+    psum[i]=psum[i-1]+a[i];
+}
+for (int i = 0; i < 10; i++)
+{
+    cout<<psum[i]<<" ";
+}
 
   ll t=1;
   cin>>t;
