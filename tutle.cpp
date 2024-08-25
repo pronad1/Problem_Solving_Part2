2da5d92b8c377872df23dc90d43af04983b68b7e
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-25 20:55:09 +0600

    8/26/24

---
+++ tutle.cpp
@@ -0,0 +1,47 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    set<char>st;
+    for(int i=0;i<n;i++)
+    {
+    st.insert(s[i]);
+    }
+    if (st.size()==1||s[0]==s[2])
+    {
+        cout<<"NO"<<'\n';
+        return;
+    }
+    cout<<"YES"<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
