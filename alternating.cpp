ec14e0c35be75126661dd6833aeb88e88088e239
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-29 23:21:52 +0600

    8/28/24

---
+++ alternating.cpp
@@ -0,0 +1,50 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    ll sum=0;
+    for (int i = 1; i <=n; i++)
+    {
+        int x;
+        cin>>x;
+        if (i%2)
+        {
+            sum+=x;
+        }
+        else
+        {
+            sum-=x;
+        }
+        
+    }
+    cout<<sum<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
