b5d132f81707e74ff8cab86793769f35dd012e37
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-30 17:04:11 +0600

    8/30/24

--- less_more.cpp
+++ less_more.cpp
@@ -1,66 +1,51 @@
-//Author  :  PROSENJIT MONDOL
-
+// Author  :  PROSENJIT MONDOL
 
 #include<bits/stdc++.h>
-#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
-#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
-const int inf = 3e5;
-using ll=long long ;
+
+using ll = long long;
 
 const ll maxn = 1000000;
 const ll mod = 1000000007;
-//------------------------------------------------------------------------------
-vector<ll>div(maxn+1,0);
 
-void solve(void)
-{
-    for (ll i = 2; i <=maxn; i++)
-    {
-        if (div[i]==0)
-        {
-            for (ll j = i; j <=maxn;j+=i)
-            {
-                
-                ll temp=j;
-                ll cnt=0;
-                while (temp%i==0)
-                {
-                    temp/=i;
+// Declare a vector with a new name to avoid conflicts
+vector<ll> divisors(maxn + 1, 0); // Renamed "div" to "divisors"
+
+// Function to compute the number of divisors or other properties
+void solve(void) {
+    // Precompute values up to maxn
+    for (ll i = 2; i <= maxn; i++) {
+        if (divisors[i] == 0) { // Found a prime number
+            for (ll j = i; j <= maxn; j += i) {
+                ll temp = j;
+                ll cnt = 0;
+                while (temp % i == 0) {
+                    temp /= i;
                     cnt++;
                 }
-                if (div[j]==0)
-                {
-                    div[j].push_back(cnt+1);
+                if (divisors[j] == 0) {
+                    divisors[j] = cnt + 1; // Start with the first divisor count
+                } else {
+                    divisors[j] *= (cnt + 1); // Multiply with existing divisor count
                 }
-                else
-                {
-                    div[j].push_back(div[j]*(cnt+1));
-                }
-                
             }
-            
         }
-        
     }
-    
 }
 
+// Main function
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-//------------------------------------------------------------------------------
-int main()
-{
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-solve();
+    solve(); // Precompute before input handling
 
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       ll n;
-       cin>>n;
-       cout<<div[n]%mod<<'\n';
-     }
-return 0;
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        cout << divisors[n] % mod << '\n'; // Output precomputed value for n
+    }
+    return 0;
 }
