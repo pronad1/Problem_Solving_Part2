41a6b21244011bc8f821a0d3436d431bdd08f32f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-09-01 21:39:30 +0600

    9/1/24

---
+++ longest_good.cpp
@@ -0,0 +1,37 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll l,r;
+    cin>>l>>r;
+    ll dif=r-l;
+    ll k=(1+sqrt(1+8*dif))/2;
+    cout<<k<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
