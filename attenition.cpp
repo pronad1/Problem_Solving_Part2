8939cf8d3605cf2dc78c5f5b5517fc6743ccbf28
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-09 11:12:13 +0600

    11/9/24

---
+++ attenition.cpp
@@ -0,0 +1,38 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n,t;
+    cin>>n>>t;
+    int r=n-1;
+    t=t-r;
+    int p=t/n;
+    cout<<p<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
