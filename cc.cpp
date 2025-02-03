8cc3b83bbba4cbe048448096467e8f3bf5d9b459
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-03 13:55:00 +0600

    3/2/25

--- cc.cpp
+++ cc.cpp
@@ -5,7 +5,6 @@
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
-const int inf = 3e5;
 using ll=long long ;
 ll mod = 1e9 + 7;
 
