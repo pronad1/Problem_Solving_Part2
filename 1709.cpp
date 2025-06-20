ec9770eee76f865c31133a8cdb02f1080d0fbe66
prosenjit <ug2102049@cse.pstu.ac.bd>
2025-06-20 17:49:33 +0600

    6/20/25

--- 1709.cpp
+++ 1709.cpp
@@ -1,11 +1,11 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
@@ -34,71 +34,63 @@ void solve(void)
     }
 
     vector<pair<int, int>> pairs;
-    for (int i = 0; i < n; i++)
+    vector<int> ac(n), bc(n);
+    ac = a;
+    bc = b;
+    sort(ac.begin(), ac.end());
+    sort(bc.begin(), bc.end());
+    while (ac != a)
     {
-        if (a[i] != i + 1)
+        for (int i = 0; i < n-1; i++)
         {
-            int x = i + 1;
-            int pos_a = -1, pos_b = -1;
-            // Find position of x in vector a
-            auto it_a = find(a.begin(), a.end(), x);
-            if (it_a != a.end())
+            if (a[i]> a[i + 1])
             {
-                pos_a = distance(a.begin(), it_a);
-                // pos_a is the position of x in a
-            }
-            // Find position of x in vector b
-            auto it_b = find(b.begin(), b.end(), x);
-            if (it_b != b.end())
-            {
-                pos_b = distance(b.begin(), it_b);
-                // pos_b is the position of x in b
-            }
-
-            pair<int, int> p;
-            if (pos_a == -1)
-            {
-                p.first = 3;
-                swap(a[i], b[pos_b]);
-            }
-            else
-            {
+                pair<int, int> p;
                 p.first = 1;
+                p.second =i + 1;
+                pairs.push_back(p);
+                swap(a[i], a[i+1]);
             }
-            p.second = x;
-            pairs.push_back(p);
         }
     }
-    // for(auto j:a)
-    // {
-    //     cout<<j<<' ';
-    // }
-    // cout << '\n';
-    for (int i = 0; i < n; i++)
+    while (bc != b)
     {
-        if (b[i] != n+ i + 1)
+        for (int i = 0; i < n-1; i++)
         {
-            int x = n+i + 1;
-            int pos_b = -1;
-            auto it_b = find(b.begin(), b.end(), x);
-            if (it_b != b.end())
+            if (b[i]> b[i + 1])
             {
-                pos_b = distance(b.begin(), it_b);
-                // pos_b is the position of x in b
+                pair<int, int> p;
+                p.first = 2;
+                p.second =i + 1;
+                pairs.push_back(p);
+                swap(b[i], b[i+1]);
             }
-
-            pair<int, int> p;
-            p.first = 2;
-            p.second = i+1;
-            pairs.push_back(p);
-            swap(b[i], b[pos_b]);
         }
     }
-    // for(auto j:b)
-    // {
-    //     cout<<j<<' ';
-    // }
-    // cout << '\n';
+    
+
+    for(int i = 0; i < n; i++)
+    {
+        if (a[i] > b[i])
+        {
+            pair<int, int> p;
+            p.first = 3;
+            p.second = i + 1;
+            pairs.push_back(p);
+            swap(a[i], b[i]);
+        }
+    }
+    for(auto j:a)
+    {
+        cout<<j<<' ';
+    }
+    cout << '\n';
+
+    for(auto j:b)
+    {
+        cout<<j<<' ';
+    }
+    cout << '\n';
     cout << pairs.size() << '\n';
     for (auto &p : pairs)
     {
@@ -112,9 +104,9 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll t=1;
-    cin>>t;
-    while(t--)
+    ll t = 1;
+    cin >> t;
+    while (t--)
     {
         solve();
     }
