0ee30e00b4eec31727bc20e481ee44f8847fe951
prosenjit <ug2102049@cse.pstu.ac.bd>
2025-07-01 21:38:56 +0600

    7/1/25

--- 2123A.cpp
+++ 2123A.cpp
@@ -1,27 +1,39 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-     int n;
-     cin>>n;
-     if(n<3 || n%2!=0)
-     {
-         cout<<"Alice"<<'\n';
-     }
-     else
-     {
-        cout<<"Bob"<<'\n';
-     }
+    int n;
+    cin >> n;
+    int c0 = 0, c1 = 0, c2 = 0, c3 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 4 == 0)
+            c0++;
+        else if (i % 4 == 1)
+            c1++;
+        else if (i % 4 == 2)
+            c2++;
+        else if (i % 4 == 3)
+            c3++;
+    }
+    if (c0 != c3 || c1 != c2)
+    {
+        cout << "Alice\n";
+    }
+    else
+    {
+        cout << "Bob\n";
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -30,9 +42,9 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll t=1;
-    cin>>t;
-    while(t--)
+    ll t = 1;
+    cin >> t;
+    while (t--)
     {
         solve();
     }
