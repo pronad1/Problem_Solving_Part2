cbd066cf1880bf050d0c2e667ae1f6cfe62e3865
prosenjit <ug2102049@cse.pstu.ac.bd>
2025-07-25 21:44:50 +0600

    ok

--- 54b.cpp
+++ 54b.cpp
@@ -12,7 +12,48 @@ ll mod = 1e9 + 7;
 
 void solve(void)
 {
-     
+    string s;
+    cin >> s;
+
+    ll n = s.size();
+    if (n == 1)
+    {
+        if(s[0] == '^')
+        {
+            cout << "1\n";
+            return;
+        }
+        else
+        {
+            cout << "2\n";
+            return;
+        }
+    }
+
+
+    vector<char> st;
+
+    if(s[0]=='_'){
+        st.push_back('^');
+    }
+
+    for (int i = 0; i <n; i++)
+    {
+        if (!st.empty() && st[st.size()-1] == '_'&&s[i]!='^')
+            {
+                st.push_back('^');
+            }
+
+
+
+        st.push_back(s[i]);
+    }
+    if (st[st.size() - 1] == '_')
+    {
+        st.push_back('^');
+    }
+    
+    cout<<st.size()-n<<"\n";
 }
 
 //------------------------------------------------------------------------------
