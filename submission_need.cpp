7ce5d09d3f568ce6d25e8a95b1f2ad3268d72fc2
prosenjit <ug2102049@cse.pstu.ac.bd>
2025-08-07 21:39:59 +0600

    8/7/25

---
+++ submission_need.cpp
@@ -0,0 +1,39 @@
+// Author  :  PROSENJIT MONDOL
+
+#include <bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll = long long;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll su = accumulate(a.begin(), a.end(), 0LL) + count(a.begin(), a.end(), 0);
+    cout << su << "\n";
+}
+
+//------------------------------------------------------------------------------
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
