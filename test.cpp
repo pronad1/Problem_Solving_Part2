de56d8a6f59395dfc3a800cd121a46cb85a559e5
prosenjit <ug2102049@cse.pstu.ac.bd>
2025-08-18 01:49:23 +0600

    ok

--- test.cpp
+++ test.cpp
@@ -6,8 +6,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    const int N = 1000000; // 1 million
-    int arr[N];            // Static allocation (stack)
+    const int N = 100000; // 1 million
+    int arr[N];           // Static allocation (stack)
     cout << "Array of size " << N << " created on stack.\n";
     return 0;
 }
