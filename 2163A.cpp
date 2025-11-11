aed18e883601ee6f84f47a00cc4d9f1e6d61d7ca
prosenjit <ug2102049@cse.pstu.ac.bd>
2025-11-11 08:50:47 +0600

    cp

---
+++ 2163A.cpp
@@ -0,0 +1,56 @@
+// Author  :  PROSENJIT MONDOL
+
+#include <bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll = long long;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    bool flag = true;
+    for (int i = 1; i < n - 1; i += 2)
+    {
+        if (a[i] != a[i + 1])
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+//------------------------------------------------------------------------------
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
