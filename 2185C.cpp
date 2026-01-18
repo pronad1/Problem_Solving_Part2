e775d1bef7a08de542299c26cd825ac608bb68ef
prosenjit <ug2102049@cse.pstu.ac.bd>
2026-01-18 21:44:19 +0600

    contest

--- 2185C.cpp
+++ 2185C.cpp
@@ -12,6 +12,36 @@ ll mod = 1e9 + 7;
 
 void solve(void)
 {
+    ll n;
+    cin >> n;
+    set<ll> a;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        a.insert(x);
+    }
+    vector<ll> a_vec(a.begin(), a.end());
+    sort(a_vec.begin(), a_vec.end());
+
+    ll f = 0, ans = 0, st = a_vec[0];
+    for (ll i = 0; i < n; i++)
+    {
+        if (a_vec[i] == st)
+        {
+            f++;
+            st++;
+        }
+        else
+        {
+            f = 1;
+            st = a_vec[i];
+            st++;
+        }
+
+        ans = max(ans, f);
+    }
+    cout << ans << "\n";
 }
 
 //------------------------------------------------------------------------------
